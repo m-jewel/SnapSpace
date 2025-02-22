1e20b6ef2e01c9e48bab60b39adb05b83bdf51c2
m-jewel <wzt206@usask.ca>
2025-02-22 13:40:29 -0600

    feat: project structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'renderer.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-react'] }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
